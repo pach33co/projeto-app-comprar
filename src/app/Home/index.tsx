13f074da5d898a7216a98a45955177a3fae3dca4
Matheus Pacheco <pachecmm@gmail.com>
2026-09-09 19:15:25 -0300

    feat: cria o componente item

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
+import { Item } from "@/components/Item";
 
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
@@ -31,6 +32,23 @@ export function Home() {
           </TouchableOpacity>
 
         </View>
+
+        <Item data={{ status: FilterStatus.DONE, description: "Café" }}
+          onRemove={() => alert("Remover")}
+          onStatus={() => alert("Mudar o status")}
+        />
+        <Item data={{ status: FilterStatus.DONE, description: "Tomate" }}
+          onRemove={() => alert("Remover")}
+          onStatus={() => alert("Mudar o status")}
+        />
+        <Item data={{ status: FilterStatus.PENDING, description: "Sabonete" }}
+          onRemove={() => alert("Remover")}
+          onStatus={() => alert("Mudar o status")}
+        />
+        <Item data={{ status: FilterStatus.PENDING, description: "Pipoca" }}
+          onRemove={() => alert("Remover")}
+          onStatus={() => alert("Mudar o status")}
+        />
       </View>
 
     </View>
---
+++ src/components/Item/index.tsx
@@ -0,0 +1,35 @@
+import { FilterStatus } from "@/types/FilterStatus"
+import { Text, TouchableOpacity, View } from "react-native"
+import { styles } from "./styles"
+import { StatusIcon } from "../StatusIcon"
+import { Trash2 } from "lucide-react-native"
+
+
+type ItemData = {
+    status: FilterStatus
+    description: string
+}
+
+type TItemProps = {
+    data: ItemData
+    onRemove: () => void
+    onStatus: () => void
+}
+
+export function Item({ data, onStatus, onRemove }: TItemProps) {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity activeOpacity={0.8} onPress={onStatus}>
+                <StatusIcon status={data.status} />
+            </TouchableOpacity>
+
+            <Text style={styles.description}>
+                {data.description}
+            </Text>
+
+            <TouchableOpacity onPress={onRemove}>
+                <Trash2  size={18} color="#CCCCCC"/>
+            </TouchableOpacity>
+        </View>
+    )
+}
