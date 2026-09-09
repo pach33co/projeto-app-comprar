da3af4a3e71079e78626192beb8feea1f5a4ea6b
Matheus Pacheco <pachecmm@gmail.com>
2026-09-09 07:45:50 -0300

    feat: cria componente de input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <Input placeholder="O que você precisa comprar?" />
       <Button title="Adicionar" />
     </View>
   )
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#E7E7E7",
+        paddingHorizontal: 16,
+        height: 48,
+        width: "100%",
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: "#9A9A9A"
+    }
+})
