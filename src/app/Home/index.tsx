926bf1dc3a541b6072d047622c5ddec14bd48f89
Matheus Pacheco <pachecmm@gmail.com>
2026-09-10 08:51:05 -0300

    feat: instala o async storage para armazenar funções

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -9,25 +9,35 @@ import { useState } from "react";
 
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-const ITEMS = [
-  { id: "1", status: FilterStatus.DONE, description: "1 pacote de café" },
-  { id: "2", status: FilterStatus.PENDING, description: "3 pacotes de macarrão" },
-  { id: "3", status: FilterStatus.PENDING, description: "3 cebolas" },
-  { id: "4", status: FilterStatus.DONE, description: "6 laranjas" },
-  { id: "5", status: FilterStatus.PENDING, description: "1 pote de açaí natural" }
-]
+
 
 export function Home() {
   const [filter, setFilter] = useState(FilterStatus.PENDING)
+  const [description, setDescription] = useState("")
+  const [items, setItems] = useState<any>([])
 
+  function handleAdd() {
+    if(!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar")
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING
+    }
+
+  }
 
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Adicionar" />
+        <Input placeholder="O que você precisa comprar?"
+        onChangeText={setDescription}
+        />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
@@ -49,7 +59,7 @@ export function Home() {
 
 
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
             <Item
