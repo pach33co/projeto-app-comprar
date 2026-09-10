c4dd7f839551e6e60d0c4df5ca86cf26a13f3322
Matheus Pacheco <pachecmm@gmail.com>
2026-09-10 06:29:45 -0300

    feat: utiliza a função useState para gerenciar a opacidade de filtros

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -5,18 +5,22 @@ import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
+import { useState } from "react";
 
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 const ITEMS = [
-  { id: "1", status: FilterStatus.DONE, description: "1 pacote de café"},
-  { id: "2", status: FilterStatus.PENDING, description: "3 pacotes de macarrão"},
-  { id: "3", status: FilterStatus.PENDING, description: "3 cebolas"},
-  { id: "4", status: FilterStatus.DONE, description: "6 laranjas"},
-  { id: "5", status: FilterStatus.PENDING, description: "1 pote de açaí natural"}
+  { id: "1", status: FilterStatus.DONE, description: "1 pacote de café" },
+  { id: "2", status: FilterStatus.PENDING, description: "3 pacotes de macarrão" },
+  { id: "3", status: FilterStatus.PENDING, description: "3 cebolas" },
+  { id: "4", status: FilterStatus.DONE, description: "6 laranjas" },
+  { id: "5", status: FilterStatus.PENDING, description: "1 pote de açaí natural" }
 ]
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING)
+
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
@@ -30,7 +34,11 @@ export function Home() {
         <View style={styles.header}>
           {
             FILTER_STATUS.map((status) => (
-              <Filter key={status} status={status} isActive />
+              <Filter key={status}
+                status={status}
+                isActive={status === filter}
+                onPress={() => setFilter(status)}
+              />
             ))
           }
 
